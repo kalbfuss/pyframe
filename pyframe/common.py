31b721c0fe1acd29c1dd92ae962f9454fd48dc38
Bernd Kalbfuss <langweiler@rueblitorte.de>
2023-04-05 19:03:24 +0200

    Changed name of parameter 'exists' to 'required'.

--- pyframe/common.py
+++ pyframe/common.py
@@ -46,7 +46,7 @@ def check_valid_required(config, valid_keys, required_keys):
     if not required_keys.issubset(keys):
         raise ConfigError(f"As a minimum, the parameters {required_keys} are required, but the parameter(s) {required_keys.difference(keys)} has/have not been specified.")
 
-def check_param(name, value, recurse=False, exists=True, is_int=False, is_color=False, gr=None, ge=None, lo=None, le=None, options=None):
+def check_param(name, value, recurse=False, required=True, is_int=False, is_color=False, gr=None, ge=None, lo=None, le=None, options=None):
     """Check validity of configuration parameter.
 
     Checks the validity of a configuration parameter based on specified
@@ -67,8 +67,8 @@ def check_param(name, value, recurse=False, exists=True, is_int=False, is_color=
     :type value: type of parameter value or dict
     :param recurse: True if function shall recurse into sets and lists
     :type recurse: bool
-    :param exists: True if parameter must have value
-    :type exists: bool
+    :param required: True if parameter must have value
+    :type required: bool
     :param is_int: True if value must be integer
     :type is_int: bool
     :param is_color: True if value must be color definition
@@ -91,9 +91,9 @@ def check_param(name, value, recurse=False, exists=True, is_int=False, is_color=
 
     # Check for non-type and empty lists/sets.
     if value is None or (isinstance(value, (list,set)) and len(value) ==0):
-        # Return immediately if value is optional and not defined.
-        if exists is False: return
-        # Ensure that value exists.
+        # Return immediately if value is not required.
+        if required is False: return
+        # Rais exception otherwise.
         else: raise ConfigError(f"No value for parameter '{name}' specified.", config)
 
     # Compile generic error message prefix.
@@ -113,7 +113,7 @@ def check_param(name, value, recurse=False, exists=True, is_int=False, is_color=
     if isinstance(value, (list,set)):
         if recurse:
             for v in value:
-                check_param(name, v, False, exists, is_int, is_color, gr, ge, lo, le, options)
+                check_param(name, v, False, required, is_int, is_color, gr, ge, lo, le, options)
         # Prevent all further tests.
         return
 
