f292ab45dc0c783918323751e7d1b706c1d2ffb5
Bernd Kalbfuss <langweiler@rueblitorte.de>
2023-04-17 23:26:45 +0200

    Housekeeping and documentation updates.

--- repository/local/file.py
+++ repository/local/file.py
@@ -1,7 +1,6 @@
 """Module for local repository files."""
 
 import logging
-import os
 import os.path
 import repository
 
@@ -12,22 +11,39 @@ from datetime import datetime
 class RepositoryFile(repository.RepositoryFile):
     """File within a local repository.
 
-    For images the following properties are read out from EXIF tag if available:
-    width, height, rotation, creation_date, description, rating, tags.
+    For images, the following properties are extracted from EXIF/IPTC tags if
+    available: width, height, rotation, creation_date, description, rating, GPS
+    coordinates, and tags (keywords).
 
     See repository.File for documentation of properties.
     """
 
     def __init__(self, uuid, rep, index=None, index_lookup=True, extract_metadata=True):
-        """Initialize file."""
+        """Initialize the repository file.
+
+        :param rep: local repository
+        :type rep: repository.local.Repository
+        :param uuid: UUID of repository file
+        :type uuid: str
+        :param index: Optional file metadata index. Default is None.
+        :type index: repository.Index
+        :param index_lookup: Flag indicating whether file metadata shall be
+            looked up from index. Default is True.
+        :type index_lookup: bool
+        :param extract_metadata: Flag indicating whether file metadata shall be
+            extracted from file if not available from index. Default is True.
+        :type extract_metadata: bool
+        :raises: repository.UuidError, repository.IoError
+        """
+        # Call constructor of parent class.
         super().__init__(uuid, rep, index, index_lookup)
 
-        # Throw exception if file does not exist
+        # Throw exception if file does not exist.
         self._path = os.path.join(rep.root, uuid)
         if not os.path.isfile(self._path):
             raise UuidError("There is no file with UUID '{uuid}'.", uuid)
 
-        # Set file name from uuid
+        # Set file name from uuid.
         self._name = os.path.basename(uuid)
 
         # Determine last modification and file creation date.
@@ -40,22 +56,6 @@ class RepositoryFile(repository.RepositoryFile):
         if (not self._in_index or self.last_updated < last_modified) and extract_metadata:
             self.extract_metadata()
 
-    def __enter__(self):
-        """Return file-like object for access to file content.
-
-        Note that this function is not thread-safe. Function __exit__ must be
-        called prior to calling __enter__ again.
-
-        :return: File-like object providing access to file content.
-        :rtype: file
-        """
-        self._file = open(self._path, "rb")
-        return self._file
-
-    def __exit__(self, type, value, traceback):
-        """Close file-like object previously returned."""
-        self._file.close()
-
     def extract_metadata(self):
         """Extract metadata from file content."""
         logging.debug(f"Extracting metadata of file '{self.uuid}' from file content.")
@@ -67,9 +67,9 @@ class RepositoryFile(repository.RepositoryFile):
 
     @property
     def source(self):
-        """Return the full path of the file.
+        """Return full path of the file.
 
-        :return: Full path of file.
+        :return: full path
         :rtype: str
         """
         return self._path
--- repository/local/repository.py
+++ repository/local/repository.py
@@ -5,7 +5,7 @@ import os.path
 import logging
 import repository
 
-from repository import ConfigError, IoError, check_valid_required
+from repository import ConfigError, IoError, check_param, check_valid_required
 
 from .file import RepositoryFile
 
@@ -18,58 +18,63 @@ class Repository(repository.Repository):
     CONF_VALID_KEYS = set() | CONF_REQ_KEYS
 
     def __init__(self, uuid, config, index=None):
-        """Initialize repository with local file base.
+        """Initialize the repository.
 
-        :param uuid: UUID of the repository.
+        :param uuid: UUID of repository
         :type name: str
-        :param root: Root directory of the repository.
-        :type root: str
+        :param config: dictionary with repository configuration
+        :type config: dict
         :param index: Optional file metadata index. Default is None.
         :type index: repository.Index
-        :raises: UuidError
+        :raises: repository.UuidError, repository.ConfigError
         """
-        # Call constructor of parent class
+        # Call constructor of parent class.
         repository.Repository.__init__(self, uuid, config, index)
-
         # Basic initialization.
         self._root = config['root']
 
-    def iterator(self, index_lookup=True, extract_metadata=True):
-        """Provide iterator which allows to traverse through all files in the repository.
+    def _check_config(self, config):
+        """Check the repository configuration.
 
-        :param index_lookup: True if file metadata shall be looked up from index.
+        :param config:
+        :type config: dict
+        :raises: repository.ConfigError
+        """
+        # Make sure valid and required parameters have been specified.
+        check_valid_required(config, self.CONF_VALID_KEYS, self.CONF_REQ_KEYS)
+        # Check parameter values.
+        check_param('root', config, is_str=True)
+
+
+    def iterator(self, index_lookup=True, extract_metadata=True):
+        """Provide iterator to traverse through files in the repository.
+
+        :param index_lookup: True if file metadata shall be looked up from index
         :type index_lookup: bool
         :param extract_metadata: True if file metadata shall be extracted from
-            file if not available from index.
-        :return: File iterator.
+            file if not available from index
+        :return: file iterator
         :return type: repository.FileIterator
         """
         return FileIterator(self, index_lookup, extract_metadata)
 
-    def _check_config(self, config):
-        """Check the configuration for the repository from the configuration
-        file.
-
-        :param config:
-        :type config: dict
-        :raises: ConfigError
-        """
-        # Make sure valid and required parameters have been specified.
-        check_valid_required(config, self.CONF_VALID_KEYS, self.CONF_REQ_KEYS)
-
     def file_by_uuid(self, uuid, index_lookup=True, extract_metadata=True):
-        """Return a file within the repository by its UUID.
+        """Return file within the repository by its UUID.
 
-        :param uuid: UUID of the file.
+        Raises a UuidError if the file with UUID does not exist. And raises an
+        IoError if the file cannot be accessed.
+
+        :param uuid: UUID of repository file
         :type uuid: str
-        :param index_lookup: True if file metadata shall be looked up from index.
+        :param index_lookup: Flag indicating whether file metadata shall be
+            looked up from index. Default is True.
         :type index_lookup: bool
-        :param extract_metadata: True if file metadata shall be extracted from
-            file if not available from index.
+        :param extract_metadata: Flag indicating whether file metadata shall be
+            extracted from file if not available from index. Default is True.
         :type extract_metadata: bool
-        :return: File with matching UUID.
+        :return: file with matching UUID
         :rtype: repository.RepositoryFile
-        :raises: UuidError
+        :raises: repository.UuidError, repository.IoError
         """
         return RepositoryFile(uuid, self, self._index, index_lookup, extract_metadata)
 
@@ -77,42 +82,44 @@ class Repository(repository.Repository):
     def root(self):
         """Return root directory of the repository.
 
-        :return: Root directory of repository.
+        :return: root directory
         :rtype: str
         """
         return self._root
 
 
 class FileIterator(repository.FileIterator):
-    """Iterator which can be used to traverse through files in a repository with local file base."""
+    """Iterator to traverse through files in a local repository."""
 
     def __init__(self, rep, index_lookup=True, extract_metadata=True):
         """Initialize file iterator.
 
-        :param root: Repository with local file base.
-        :type root: repository.local.repository
-        :param index_lookup: True if file metadata shall be looked up from index.
+        :param rep: local repository
+        :type rep: repository.local.Repository
+        :param index_lookup: Flag indicating whether file metadata shall be
+            looked up from index. Default is True.
         :type index_lookup: bool
-        :param extract_metadata: True if file metadata shall be extracted from
-            file if not available from index.
+        :param extract_metadata: Flag indicating whether file metadata shall be
+            extracted from file if not available from index. Default is True.
         :type extract_metadata: bool
-        :raise: repository.IOError
+        :raises: repository.IoError
         """
+        # Basic initialization.
         self._rep = rep
         self._index_lookup = index_lookup
         self._extract_metadata = extract_metadata
         self._dir_list = []
-        # Create scandir iterator for provided root directory
+        # Create scandir iterator for provided root directory.
         try:
             self._iterator = os.scandir(self._rep.root)
         except Exception as e:
-            raise IOError("An exception occurred while scanning directory:", e)
+            raise IoError(f"An exception occurred while scanning the root directory. {e}.", e)
 
     def __next__(self):
-        """Provide the next file.
+        """Provide next file in iteration.
 
-        :returns: Next file in the repository.
-        :rtype: repository.local.RepositoryFile
+        :returns: next file
+        :rtype: repository.rclone.RepositoryFile
         :raises: StopIteration
         """
         try:
@@ -128,16 +135,17 @@ class FileIterator(repository.FileIterator):
             # Construct relative path to root directory of the repository.
             uuid = os.path.relpath(entry.path, start=self._rep.root)
             # Return the next file.
-            logging.debug(f"Creating next local repository file {uuid}.")
+            logging.debug(f"Creating local repository file {uuid}.")
             return self._rep.file_by_uuid(uuid, self._index_lookup, self._extract_metadata)
 
         except StopIteration:
             if len(self._dir_list) > 0:
-                # Start all over with first subdirectory in the list
+                # Start all over with first subdirectory in the list.
                 try:
-                    self._iterator = os.scandir(self._dir_list.pop())
+                    dir = self._dir_list.pop()
+                    self._iterator = os.scandir(dir)
                 except Exception as e:
-                    raise IOError("An exception occurred while scanning directory:", e)
+                    raise IoError(f"An exception occurred while scanning directory '{dir}'. {e}", e)
                 return self.__next__()
             else:
                 # Raise exception to indicate end of iteration otherwise
